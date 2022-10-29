6591358b9154bba9a0626952243c8d075d0f0818
jovinkassi <jovinchoondal2022@gmail.com>
2022-10-29 21:46:56 +0400

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,32 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
 //require("./tasks/block-number");
-require("@nomiclabs/hardhat-etherscan");
-require("dotenv").config();
-require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
+require("solidity-coverage")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const Goerli_RPC_URL = process.env.Goerli_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.7",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: Goerli_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    solidity: "0.8.7",
+}
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners()
+
+    for (const account of accounts) {
+        console.log(account.address)
+    }
+})
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,18 @@
-async function main() {}
+//imports 
+
+const {ethers} = require("hardhat")
+
+
+async function main() {
+
+  const SimpleStorageFactory = await ethers.getContractFactory(
+    "SimpleStorage"
+  )
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.deployed()
+  console.log(`Deployed contract to: ${simpleStorage.address}`)
+}
 
 main()
   .then(() => process.exit(0))
